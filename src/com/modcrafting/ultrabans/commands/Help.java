ddb1f5f2ef59f9259b087460efac5973c11a613d
deathmarine <josh@mcjoshmod.com>
2011-11-28 18:02:38 -0500

    Changed Version Info

--- src/com/modcrafting/ultrabans/commands/Help.java
+++ src/com/modcrafting/ultrabans/commands/Help.java
@@ -5,8 +5,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.PluginDescriptionFile;
-
 import com.modcrafting.ultrabans.UltraBan;
 import com.nijikokun.bukkit.Permissions.Permissions;
 
@@ -32,29 +30,28 @@ public class Help implements CommandExecutor{
 			server = true;
 		}
 		if (server){
-			sender.sendMessage(ChatColor.BLUE + "Required Info {}" + ChatColor.GREEN + " Optional ()" + ChatColor.RED + " Silent -s");
-			sender.sendMessage(ChatColor.GRAY + "/ban        {player} (-s) {reason}");
-			sender.sendMessage(ChatColor.GRAY + "/permaban   {player} (-s) {reason}");		
-			sender.sendMessage(ChatColor.GRAY + "/tempban    {player} (-s) {amt} {sec/min/hour/day} {Reason}");
-			sender.sendMessage(ChatColor.GRAY + "/ipban      {player} (-s) {reason}");
-			sender.sendMessage(ChatColor.GRAY + "/unban      {player}");
-			sender.sendMessage(ChatColor.GRAY + "/checkban   {player}");
-			sender.sendMessage(ChatColor.GRAY + "/kick       {player} (-s) {reason}");
-			sender.sendMessage(ChatColor.GRAY + "/warn       {player} (-s) {reason}");
-			sender.sendMessage(ChatColor.GRAY + "/fine       {player} (-s) {amt}");
-			sender.sendMessage(ChatColor.GRAY + "/empty      {player}");
-			sender.sendMessage(ChatColor.GRAY + "/forcespawn {player}");
-			sender.sendMessage(ChatColor.GRAY + "/starve     {player}");
-			sender.sendMessage(ChatColor.GRAY + "/editban    (help)");
-			sender.sendMessage(ChatColor.GRAY + "/lockdown   {on/off/status}");
-			sender.sendMessage(ChatColor.GRAY + "/jail (set/pardon/{player}) {player}");
-			sender.sendMessage(ChatColor.GRAY + "/uhelp /exportbans /ureload /uversion /importbans");
+			sender.sendMessage("Required Info {} Optional () Silent -s");
+			sender.sendMessage("/ban        {player} (-s) {reason}");
+			sender.sendMessage("/permaban   {player} (-s) {reason}");		
+			sender.sendMessage("/tempban    {player} (-s) {amt} {sec/min/hour/day} {Reason}");
+			sender.sendMessage("/ipban      {player} (-s) {reason}");
+			sender.sendMessage("/unban      {player}");
+			sender.sendMessage("/checkban   {player}");
+			sender.sendMessage("/kick       {player} (-s) {reason}");
+			sender.sendMessage("/warn       {player} (-s) {reason}");
+			sender.sendMessage("/fine       {player} (-s) {amt}");
+			sender.sendMessage("/empty      {player}");
+			sender.sendMessage("/forcespawn {player}");
+			sender.sendMessage("/starve     {player}");
+			sender.sendMessage("/rules      (help)");
+			sender.sendMessage("/editban    (help)");
+			sender.sendMessage("/lockdown   {on/off/status}");
+			sender.sendMessage("/jail (set/pardon/{player}) {player}");
+			sender.sendMessage("/uhelp /exportbans /importbans /ureload /uversion");
 			return true;
 		}
 		if (auth) {
-		PluginDescriptionFile pdfFile = plugin.getDescription();
-		sender.sendMessage(ChatColor.GRAY + pdfFile.getName() + " version " + pdfFile.getVersion() + " Help System");
-		sender.sendMessage(ChatColor.BLUE + "Required Info {}" + ChatColor.GREEN + " Optional ()" + ChatColor.RED + " Silent -s");
+		sender.sendMessage("Ultrabans " + ChatColor.BLUE + "Required Info {}" + ChatColor.GREEN + " Optional ()" + ChatColor.RED + " Silent -s");
 		if (Permissions.Security.permission(player, "ultraban.ban")) sender.sendMessage(ChatColor.GRAY + "/ban       {player} (-s) {reason}");
 		if (Permissions.Security.permission(player, "ultraban.permaban"))sender.sendMessage(ChatColor.GRAY + "/permaban       {player} (-s) {reason}");		
 		if (Permissions.Security.permission(player, "ultraban.tempban"))sender.sendMessage(ChatColor.GRAY + "/tempban  {player} (-s) {amt} {sec/min/hour/day} {Reason}");
@@ -67,6 +64,7 @@ public class Help implements CommandExecutor{
 		if (Permissions.Security.permission(player, "ultraban.emtpy"))sender.sendMessage(ChatColor.GRAY + "/empty    {player}");
 		if (Permissions.Security.permission(player, "ultraban.spawn"))sender.sendMessage(ChatColor.GRAY + "/forcespawn {player}");
 		if (Permissions.Security.permission(player, "ultraban.starve"))sender.sendMessage(ChatColor.GRAY + "/starve     {player}");
+		if (Permissions.Security.permission(player, "ultraban.rules"))sender.sendMessage(ChatColor.GRAY + "/rules     (help)");
 		if (Permissions.Security.permission(player, "ultraban.editban"))sender.sendMessage(ChatColor.GRAY + "/editban  (help)");
 		if (Permissions.Security.permission(player, "ultraban.jail"))sender.sendMessage(ChatColor.GRAY + "/jail (set/pardon/{player}) {player}");
 		if (Permissions.Security.permission(player, "ultraban.lockdown"))sender.sendMessage(ChatColor.GRAY + "/lockdown   {on/off/status}");
