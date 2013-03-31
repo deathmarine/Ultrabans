352143e69459abefa9ae1960a09828fb9478d8cf
deathmarine <josh@modcrafting.com>
2013-03-31 19:27:56 -0400

    Lang update.

--- src/com/modcrafting/ultrabans/listeners/UltraBanPlayerListener.java
+++ src/com/modcrafting/ultrabans/listeners/UltraBanPlayerListener.java
@@ -30,7 +30,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
